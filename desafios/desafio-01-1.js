347df97aa75f255c46e3ee082013dfc867441e7a
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-01-28 13:32:19 -0300

    Desafio

--- desafios/desafio-01-1.js
+++ desafios/desafio-01-1.js
@@ -7,10 +7,10 @@ const altura = 1.80
 const imc = peso / (altura * altura)
 
 if (imc >= 30) {
-    console.log(`${nome} você está acima do peso`)
+    console.log(`${nome}, você está acima do peso`)
 } else {
     if (imc < 29.9) {
-        console.log(`${nome} você não está acima do peso`)
+        console.log(`${nome}, você não está acima do peso`)
     }
 }
 
