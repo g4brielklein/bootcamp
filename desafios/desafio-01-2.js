0ae7b332e8e0bbb3adeea1cd96b0d6a181a3cf3c
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-01-28 13:30:21 -0300

    Desafio

--- desafios/desafio-01-2.js
+++ desafios/desafio-01-2.js
@@ -32,4 +32,3 @@ const programador = {
 }
 
 console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade}`)
-
