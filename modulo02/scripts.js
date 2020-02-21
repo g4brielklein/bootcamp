50b18be8900cddb6ee0937afc1e7b31b0ffa8089
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-02-20 23:22:06 -0300

    Iniciando no Front-end

--- modulo02/scripts.js
+++ modulo02/scripts.js
@@ -3,6 +3,7 @@ const cards = document.querySelectorAll('.card')
 
 for(let card of cards) {
     card.addEventListener("click", function(){
+        const videoId = card.getAttribute("id");
         modalOverlay.classList.add('active')
         modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
     })
@@ -10,4 +11,5 @@ for(let card of cards) {
 
 document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector("iframe").src = ""
 })
