ab2dd38daa1d445840124775bd0d14d3ff2c8e2e
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-02-22 21:54:58 -0300

    Desafio 02-3

--- desafios/desafio-02-3-refeito/scripts.js
+++ desafios/desafio-02-3-refeito/scripts.js
@@ -1,12 +1,10 @@
 const modalOverlay = document.querySelector(".modal-overlay")
-const modal = document.querySelector(".modal")
 const cursos = document.querySelectorAll(".curso")
 
 for(let curso of cursos) {
     curso.addEventListener("click", function(){
         const cursoId = curso.getAttribute("id")
         modalOverlay.classList.add("active")
-        modal.classList.add("maximize")
         modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${cursoId}`
     })
 }
@@ -14,4 +12,9 @@ for(let curso of cursos) {
 document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove('active')
     modalOverlay.querySelector("iframe").src = ""
+})
+
+document.querySelector(".maximize-modal").addEventListener("click", function(){
+    alert("Funcionando!")
+    modalOverlay.classList.contains("maximize")
 })
