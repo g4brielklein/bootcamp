43fa7c6582bccca2b1fa4c12b5a34affee0e77a3
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-02-25 14:55:42 -0300

    Desafio 02-3

--- desafios/desafio-02-3/scripts.js
+++ desafios/desafio-02-3/scripts.js
@@ -1,27 +1,27 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const modal = document.querySelector('.modal')
-const cursos = document.querySelectorAll('.curso')
+const modalOverlay = document.querySelector(".modal-overlay")
+const cursos = document.querySelectorAll(".curso")
+const modal = document.querySelector(".modal")
 
-for (let curso of cursos) {
+for(let curso of cursos) {
     curso.addEventListener("click", function(){
-        modal.classList.remove('maximize')
-        modalOverlay.classList.add('active')
-        const cursoId = curso.getAttribute('id')
-        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${cursoId}`
+        modal.classList.remove("maximize")
+        const cursoId = curso.getAttribute("id")
+        modalOverlay.classList.add("active")
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${cursoId}`
     })
 }
 
-document.querySelector('.close-modal').addEventListener("click", function(){
+document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove('active')
-    modalOverlay.querySelector('iframe').src = ""
+    modalOverlay.querySelector("iframe").src = ""
 })
 
-document.querySelector('.maximize-modal').addEventListener("click", function(){
-    is_maximize = modal.classList.contains('maximize')
-    if (is_maximize) {
+document.querySelector(".maximize-modal").addEventListener("click", function(){
+    const is_maximize = modal.classList.contains('maximize')
+    if(is_maximize) {
         modal.classList.remove('maximize')
     } else {
         modal.classList.add('maximize')
     }
-    
 })
+    
