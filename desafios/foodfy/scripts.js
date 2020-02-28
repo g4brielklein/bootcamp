361ea69a3a8400851cb6f573bb45adaaf8c5d145
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-02-28 10:27:43 -0300

    Trabalhando no iframe do modal

--- desafios/foodfy/scripts.js
+++ desafios/foodfy/scripts.js
@@ -1,1 +1,23 @@
-alert("Teste");
+const modalOverlay = document.querySelector(".modal-overlay")
+const receitas = document.querySelectorAll(".receita")
+const modal = document.querySelector(".modal")
+
+for(let receita of receitas) {
+    receita.addEventListener("click", function(){
+        modal.classList.remove("maximize")
+        modalOverlay.classList.add("active")
+    })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active")
+})
+
+document.querySelector(".maximize-modal").addEventListener("click", function(){
+    const is_maximize = modal.classList.contains("maximize")
+    if(is_maximize) {
+        modal.classList.remove("maximize")
+    } else {
+        modal.classList.add("maximize")
+    }
+})
