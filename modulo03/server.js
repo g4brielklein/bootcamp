c0966ace6aad9630401dc082a10650e2713ccaca
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-03-08 00:47:44 -0300

    Configurando Nunjucks

--- modulo03/server.js
+++ modulo03/server.js
@@ -1,9 +1,18 @@
 const express = require('express')
-const server = express()
 const nunjucks = require('nunjucks')
 
+const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express:server
+})
+
 server.get("/", function(req, res) {
-    return res.send("Hi! How's going?")
+    return res.render("index")
 })
 
 server.listen(5000, function(){
