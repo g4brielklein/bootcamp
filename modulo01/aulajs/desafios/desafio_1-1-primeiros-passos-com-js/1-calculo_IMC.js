602bcc85a5fe3b218b472508497d0c479d43632b
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-07-27 10:42:06 -0300

    Desafio 1-1: Primeiros passos com JS

--- modulo01/aulajs/desafios/desafio_1-1-primeiros-passos-com-js/1-calculo_IMC.js
+++ modulo01/aulajs/desafios/desafio_1-1-primeiros-passos-com-js/1-calculo_IMC.js
@@ -1,7 +1,8 @@
 // Desafio 1-1: Primeiros passos com JS
 
 // Crie um programa para calcular o IMC e nível de obesidade de uma pessoa.
-// Comece criando constantes para armazenar o nome, peso, altura e sexo de uma pessoa.
+
+// Comece criando constantes para armazenar o nome, peso e altura de uma pessoa.
 const nome = 'Gabriel'
 const peso = 80
 const altura = 1.80
@@ -12,9 +13,10 @@ const imc = peso / ( altura * altura )
 
 // Baseado no valor obtido através desse cálculo exiba as seguintes mensagens:
 // SE o IMC maior ou igual a 30: Carlos você está acima do peso;
-// SE o IMC menor que 29.9: Carlos você não está acima do peso;
 if (imc >= 30) {
     console.log(`${nome}, você está acima do peso!`)
-} else {
+}
+// SE o IMC menor que 29.9: Carlos você não está acima do peso;
+else {
     console.log(`${nome}, você não está acima do peso.`)
 }
