fdfb218aba58f573ee211040e8054cb74a3cc23c
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-08-04 10:25:43 -0300

    Desafio 2-3: Página de cursos e iframe

--- modulo02/desafios/desafio-2-3-pagina-de-cursos-e-iframe/script.js
+++ modulo02/desafios/desafio-2-3-pagina-de-cursos-e-iframe/script.js
@@ -1,21 +1,22 @@
-const modal = document.querySelector('.modal')
 const cards = document.querySelectorAll('.card')
+const modalOverlay = document.querySelector('.modal-overlay')
+const modal = modalOverlay.querySelector('.modal')
 
 for (let card of cards) {
     card.addEventListener('click', function() {
+        modalOverlay.classList.add('active')
         modal.classList.remove('maximize')
         const cursoId = card.getAttribute('id')
-        modal.classList.add('active')
-        modal.querySelector('iframe').src = `https://rocketseat.com.br/${cursoId}`
+        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${cursoId}`
     })
 }
 
-modal.querySelector('.close-modal').addEventListener('click', function() {
-    modal.classList.remove('active')
-    modal.querySelector('iframe').src = ''
+modalOverlay.querySelector('.close-modal').addEventListener('click', function() {
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ''
 })
 
-modal.querySelector('.maximize-modal').addEventListener('click', function() {
+modalOverlay.querySelector('.maximize-modal').addEventListener('click', function() {
     if (modal.classList.contains('maximize')) {
         modal.classList.remove('maximize')
     } else {
