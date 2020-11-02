00250d6b49f6a514f85b0278cc99aa01bbc4fd5f
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-11-02 00:13:34 -0300

    Comentários, Strings e Numbers

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,2 +1,9 @@
-const nome = 'Gabriel Bootcamp'
-console.log(nome);
+// Criar um programa que calcula a média
+// das notas entre os alunos e envia
+// uma mensagem do cáculo da média.
+
+const aluno1 = "Gabriel"
+const aluno2 = 'Diego'
+
+const notaAluno1 = 9.8
+const notaAluno2 = 10
