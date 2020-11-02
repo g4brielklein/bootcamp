0382c8f9cd15bd9ff785a990029ec7dff8cc7efd
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-11-01 23:56:40 -0300

    Iniciando com a programação web

---
+++ modulo01/exemplo_basico.js
@@ -0,0 +1,9 @@
+const nome = 'Gabriel';
+const idade = 21;
+
+if (idade < 99) {
+    // return `Fala ${nome}, você está novo para programar.`
+    console.log(`Fala ${nome}, você está novo para programar.`)
+}
+
+// Fala Gabriel, você está novo para programar.
