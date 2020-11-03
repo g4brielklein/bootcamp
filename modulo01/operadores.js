629248b9f26797250f7efd7d3cb5c64c198868d5
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-11-02 22:08:39 -0300

    Operadores relacionais e comparativos

---
+++ modulo01/operadores.js
@@ -0,0 +1,19 @@
+/*
+console.log( 4 == "4" ) // true
+console.log( 4 === "4" ) // false
+console.log( 4 != "5" ) // true
+console.log( 4 !== "5" ) // true
+console.log( 4 !== "4" ) // false
+*/
+
+const idade = 21
+
+if (idade >= 18) {
+    console.log('Deixar entrar')
+} else {
+    console.log('Bloquear a entrada')
+}
+
+if (idade == 17) {
+    console.log('Volte quando fizer 18 anos')
+}
