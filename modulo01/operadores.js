1cd6915be0a3c1610a4cd2cacf0b0eca5c7cf331
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-11-02 22:40:21 -0300

    Operadores lógicos e aritméticos

--- modulo01/operadores.js
+++ modulo01/operadores.js
@@ -1,3 +1,4 @@
+// Operadores de comparação
 /*
 console.log( 4 == "4" ) // true
 console.log( 4 === "4" ) // false
@@ -6,6 +7,7 @@ console.log( 4 !== "5" ) // true
 console.log( 4 !== "4" ) // false
 */
 
+/*
 const idade = 21
 
 if (idade >= 18) {
@@ -17,3 +19,29 @@ if (idade >= 18) {
 if (idade == 17) {
     console.log('Volte quando fizer 18 anos')
 }
+*/
+
+// Operadores lógicos 
+console.log(5 == 5 && 6 == 6)
+console.log(5 == 5 && 6 != 6)
+
+console.log(5 != 5 || 6 == 6)
+console.log(5 == 5 || 6 != 6)
+
+console.log(!(5 > 6))
+console.log(!(5 < 6))
+
+const idade = 21
+
+if (!(idade >= 18 || idade == 17)) {
+    console.log('Bloquear a entrada')
+} else {
+    console.log('Deixar entrar')
+}
+
+// Operadores aritméticos
+console.log(2 * 2) // 4
+console.log(2 / 2) // 1
+console.log(2 % 1.5) // 0.5
+console.log(2 + 2) // 4
+console.log(2 - 2) // 0
