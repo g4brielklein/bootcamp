6b6ee5fccb038b6afb515fdcbf84dc8b5a50a8d5
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-11-07 19:34:01 -0300

    Desafio 1-3: Funções e estruturas de repetição

--- modulo01/desafio-1-3.js
+++ modulo01/desafio-1-3.js
@@ -1,69 +1,110 @@
 // Desafio 1-3: Funções e estruturas de repetição
 
-// Crie um programa que armazena um array de usuários (objetos), 
-// cada usuário tem um nome e suas tecnologias (novo array).
+// Usuários e tecnologias
+// Crie um programa que armazena um array de usuários (objetos), cada usuário 
+// tem um nome e suas tecnologias (novo array).
 const usuarios = [
-    { nome: 'Michael', tecs: ['JavaScript', 'React'] },
-    { nome: 'Dwight', tecs: ['PHP', 'Laravel'] },
-    { nome: 'Jim', tecs: ['HTML', 'CSS'] }
+    {
+        nome: 'Dwight',
+        tecnologias: ['JavaScript', 'React']
+    },
+    {
+        nome: 'Michael',
+        tecnologias: ['PHP', 'Laravel']
+    },
+    {
+        nome: 'Jim',
+        tecnologias: ['HTML', 'CSS']
+    }
 ]
 
-// Percorra a lista de usuários com uma estrutura de repetição 
-// imprimindo em tela as informações dos usuários:
-
-for (i = 0; i < usuarios.length; i++) {
-    console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecs.join(', ')}`)
+// Percorra a lista de usuários com uma estrutura de repetição imprimindo em tela as informações dos usuários:
+for (let usuario of usuarios) {
+    console.log(`${usuario.nome} trabalha com ${usuario.tecnologias.join(', ')}`)
 }
 
+console.log('==================================')
+// Busca por tecnologia
 // Baseado no desafio anterior, utilize a mesma lista de usuários construída.
-
-// Crie uma função que recebe os dados de um objeto de usuário e retorna 
-// SE o usuário trabalha com CSS ou não. Essa função deve retornar um boolean true/false.
+// Crie uma função que recebe os dados de um objeto de usuário e retorna SE o 
+// usuário trabalha com CSS ou não. Essa função deve retornar um boolean true/false.
 function checaSeTrabalhaComCSS(usuario) {
-    for (let tec of usuario.tecs) {
-        // ou (let i = 0; i < usuario.tecs.length; i++)
-        if (tec == 'CSS') {
+    for (let tecnologia of usuario.tecnologias) {
+        if (tecnologia == 'CSS') {
             return true
         }
     }
+
     return false
 }
 
+// Percorra o array de usuários e, para cada um, verifique se o mesmo trabalha com CSS 
+// utilizando a função construída acima, se SIM, imprima em tela as informações do usuário:
 for (let usuario of usuarios) {
     if (checaSeTrabalhaComCSS(usuario)) {
         console.log(`O usuário ${usuario.nome} trabalha com CSS!`)
     }
 }
 
-// Crie um programa que calcula a soma de receitas e despesas de usuários 
-// e no fim retorna o saldo (receitas - despesas).
-const funcionarios = [
-    { nome: 'Pam', receitas: [115.3, 48.7, 98.3, 14.5], despesas: [85.3, 13.5, 19.9] },
-    { nome: 'Oscar', receitas: [24.6, 214.3, 45.3], despesas: [185.3, 12.1, 120.0] },
-    { nome: 'Ryan', receitas: [9.8, 120.3, 340.2, 45.3], despesas: [450.2, 29.9] }
+console.log('==================================')
+// Soma de despesas e receitas
+// Crie um programa que calcula a soma de receitas e despesas de usuários e no fim retorna o saldo (receitas - despesas).
+// Utilize o array de usuários abaixo:
+const clientes = [
+    {
+        nome: 'Pam',
+        receitas: [115.3, 48.7, 98.3, 14.5],
+        despesas: [85.3, 13.5, 19.9]
+    },
+    {
+        nome: 'Oscar',
+        receitas: [24.6, 214.3, 45.3],
+        despesas: [185.3, 12.1, 120.0]
+    },
+    {
+        nome: 'Ryan',
+        receitas: [9.8, 120.3, 340.2, 45.3],
+        despesas: [450.2, 29.9]
+    },
+    {
+        nome: 'Andrew',
+        receitas: [1200],
+        despesas: [1200]
+    }
 ]
 
-// Percorra o array de usuários e para cada usuário chame uma função chamada calculaSaldo que 
-// recebe como parâmetro as receitas e despesas do usuário:
-for (let funcionario of funcionarios) {
-    const saldo = (calculaSaldo(funcionario.receitas, funcionario.despesas))
-    if (saldo >= 0) {
-        console.log(`${funcionario.nome} possui saldo POSITIVO de ${saldo.toFixed(2)}`)
-    } else {
-        console.log(`${funcionario.nome} possui saldo NEGATIVO de ${saldo.toFixed(2)}`)
+// Percorra o array de usuários e para cada usuário chame uma função chamada calculaSaldo 
+// que recebe como parâmetro as receitas e despesas do usuário:
+
+let saldo = 0
+
+for (let cliente of clientes) {
+    saldo = calculaSaldo(cliente.receitas, cliente.despesas)
+
+    if (saldo > 0) {
+        console.log(`${cliente.nome} possui saldo POSITIVO de ${saldo}`)
+    }
+
+    if (saldo < 0) {
+        console.log(`${cliente.nome} possui saldo NEGATIVO de ${saldo}`)
+    }
+
+    if (saldo == 0) {
+        console.log(`${cliente.nome} não possui nenhum saldo.`)
     }
 }
 
-
 function calculaSaldo(receitas, despesas) {
-    const totalReceitas = somaNumeros(receitas)
-    const totalDespesas = somaNumeros(despesas)
+    const somaReceitas = somaNumeros(receitas)
+    const somaDespesas = somaNumeros(despesas)
 
-    const saldo = totalReceitas - totalDespesas
-
-    return saldo
+    saldo = somaReceitas - somaDespesas
+    
+    return saldo.toFixed(2)
 }
 
+// Crie uma segunda função que recebe como parâmetro um array de números e retorna a soma deles 
+// e use-a para calcular a soma de receitas e despesas dentro da função calculaSaldo:
 function somaNumeros(numeros) {
     let soma = 0
 
@@ -73,3 +114,7 @@ function somaNumeros(numeros) {
 
     return soma
 }
+
+// A função calculaSaldo deve utilizar a função somaNumeros para calcular a soma de receitas 
+// e despesas e no fim retornar o saldo do usuário, ou seja receitas - despesas.
+// No fim exiba todos usuários em telas, seu respectivo saldo e SE o saldo é POSITIVO ou NEGATIVO.
