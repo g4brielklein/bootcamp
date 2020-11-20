bcd9cd78c7ecd8fc148b65b9e9c90b6d3464c05d
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-11-20 20:29:18 -0300

    Adicionando iframe dinamico com JavaScript

--- foodfy/script.js
+++ foodfy/script.js
@@ -3,10 +3,13 @@ const modalOverlay = document.querySelector('.modal-overlay')
 
 for (let recepi of recepis) {
     recepi.addEventListener('click', function() {
+        const id = recepi.getAttribute('id')
+        modalOverlay.querySelector('iframe').src = `recepis/${id}.html`
         modalOverlay.classList.add('active')
     })
 }
 
 modalOverlay.querySelector('.close-modal').addEventListener('click', function() {
+    modalOverlay.querySelector('iframe').src = ''
     modalOverlay.classList.remove('active')
 })
