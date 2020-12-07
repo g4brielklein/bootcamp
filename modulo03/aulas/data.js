c5216e536791823b74b70bffb5c9d97bbe9e3ff9
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-12-07 00:12:00 -0300

    Exportando e importando JavaScript

--- modulo03/aulas/data.js
+++ modulo03/aulas/data.js
@@ -6,33 +6,33 @@ module.exports = [
         price: 'Free'
     },
     {
-        id: 'HN1UjzRSdBk',
-        title: 'Desvendando CSS Grid na Prática',
-        duration: '36 min',
+        id: '2alg7MQ6_sI',
+        title: 'Git e GitHub na Prática',
+        duration: '33 min',
         price: 'Free'
     },
     {
-        id: 'HN1UjzRSdBk',
-        title: 'Desvendando CSS Grid na Prática',
-        duration: '36 min',
+        id: 'vqrjFnq3-uo',
+        title: 'Criando player de áudio',
+        duration: '105 min',
         price: 'Free'
     },
     {
-        id: 'HN1UjzRSdBk',
-        title: 'Desvendando CSS Grid na Prática',
-        duration: '36 min',
+        id: 'GykTLqODQuU',
+        title: 'Formulário animado com JS e CSS',
+        duration: '57 min',
         price: 'Free'
     },
     {
-        id: 'HN1UjzRSdBk',
-        title: 'Desvendando CSS Grid na Prática',
-        duration: '36 min',
+        id: 'GSqR2i-Pq6o',
+        title: 'Desvendando a variável this',
+        duration: '48 min',
         price: 'Free'
     },
     {
-        id: 'HN1UjzRSdBk',
-        title: 'Desvendando CSS Grid na Prática',
-        duration: '36 min',
+        id: 'ghTrp1x_1As',
+        title: 'O que é API? REST e RESTFUL?',
+        duration: '33 min',
         price: 'Free'
     },
 ]
