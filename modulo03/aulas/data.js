b85e5c390b02d4f8ec56361e047470ed3bc99edf
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-12-06 23:59:15 -0300

    Exportando e importando JavaScript

---
+++ modulo03/aulas/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: 'HN1UjzRSdBk',
+        title: 'Desvendando CSS Grid na Prática',
+        duration: '36 min',
+        price: 'Free'
+    },
+    {
+        id: 'HN1UjzRSdBk',
+        title: 'Desvendando CSS Grid na Prática',
+        duration: '36 min',
+        price: 'Free'
+    },
+    {
+        id: 'HN1UjzRSdBk',
+        title: 'Desvendando CSS Grid na Prática',
+        duration: '36 min',
+        price: 'Free'
+    },
+    {
+        id: 'HN1UjzRSdBk',
+        title: 'Desvendando CSS Grid na Prática',
+        duration: '36 min',
+        price: 'Free'
+    },
+    {
+        id: 'HN1UjzRSdBk',
+        title: 'Desvendando CSS Grid na Prática',
+        duration: '36 min',
+        price: 'Free'
+    },
+    {
+        id: 'HN1UjzRSdBk',
+        title: 'Desvendando CSS Grid na Prática',
+        duration: '36 min',
+        price: 'Free'
+    },
+]
