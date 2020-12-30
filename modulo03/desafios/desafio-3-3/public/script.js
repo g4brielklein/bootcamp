0e72af480b6c8746c99b1115c88db40a5dba039a
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-12-29 22:54:21 -0300

    Desafio 3-3

--- modulo03/desafios/desafio-3-3/public/script.js
+++ modulo03/desafios/desafio-3-3/public/script.js
@@ -4,7 +4,6 @@ const cards = document.querySelectorAll('.card')
 for (let card of cards) {
     card.addEventListener('click', function() {
         const id = card.getAttribute('id')
-        alert(id)
-        window.location.href = `/posts/${id}`
+        window.location.href = `posts/${id}`
     })
 }
