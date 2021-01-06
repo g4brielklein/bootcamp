4ad86c11bd2e46507bfe453c1f01370b58b5c084
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-01-06 19:28:06 -0300

    Desafio 3-1: Primeiro servidor

---
+++ modulo03/teste/desafios/desafio-3-1/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express();
+
+server.use(express.static('public'))
+
+server.set('view engine', 'njk')
+
+nunjucks.configure('views', {
+    express:server,
+    noCache: true
+})
+
+server.get('/', function(req, res) {
+    return res.render('index')
+})
+
+server.get('/about', function(req, res) {
+    return res.render('about')
+})
+
+server.get('/contents', function(req, res) {
+    return res.render('contents')
+})
+
+server.listen('5000', function() {
+    console.log('server is running')
+});
