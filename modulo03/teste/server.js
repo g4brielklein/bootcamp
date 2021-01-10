b5f14defa7078394bfda2ee6a91bda0f823c9d4f
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-01-09 21:23:49 -0300

    Atualizando página sobr

--- modulo03/teste/server.js
+++ modulo03/teste/server.js
@@ -10,15 +10,33 @@ server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
     express:server,
+    autoescape: false,
     noCache: true
 })
 
 server.get('/', function(req, res) {
-    return res.render('about')
+    const data = {
+        avatar_url: 'https://avatars0.githubusercontent.com/u/22963494?s=460&u=3f020d4a9f9041ada4585a645cd8721b99a9fcc0&v=4',
+        name: 'Gabriel Klein',
+        role: 'Data Engineer at <a href="http://cadastra.com" target="_blank">Cadastra</a>',
+        description: 'Student at Rocketseat and Information Tecnology Student at Ulbra',
+        links: [
+            {
+                url: 'https://github.com/g4brielklein',
+                name: 'GitHub'
+            },
+            {
+                url: 'https://linkedin.com/in/g4brielklein',
+                name: 'LinkedIn'
+            }
+        ]
+    }
+
+    return res.render('about', { data })
 })
 
 server.get('/portfolio', function(req, res) {
-    return res.render('portfolio')
+    return res.render('portfolio', { items: videos })
 })
 
 server.listen(5000, function() {
