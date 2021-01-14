48d1c2f8f9d9198c27776c64f0a5b5a867bc6ed9
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-01-14 12:56:01 -0300

    Redirecionando URL com JavaScript

--- modulo03/teste/public/script.js
+++ modulo03/teste/public/script.js
@@ -1,16 +1,8 @@
-const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
-const iframe = modalOverlay.querySelector('iframe')
 
 for (let card of cards) {
     card.addEventListener('click', function() {
-        modalOverlay.classList.add('active')
         const id = card.getAttribute('id')
-        iframe.src = `https://www.youtube.com/embed/${id}`
+        window.location.href = `/video?id=${id}`
     })
 }
-
-modalOverlay.querySelector('.close-modal').addEventListener('click', function() {
-    modalOverlay.classList.remove('active')
-    iframe.src = ''
-})
