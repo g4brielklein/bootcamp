e6ca349ab1fe0fd62e58f36863198e563fa392a3
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-01-25 22:09:16 -0300

    Desafio 3: Refatorando Foodfy

--- foodfy/server.js
+++ foodfy/server.js
@@ -17,16 +17,43 @@ app.get('/', function(req, res)  {
 })
 
 app.get('/about', function(req, res) {
-    return res.render('about')
+    const data = {
+        title_1: 'Sobre o Foodfy',
+        about: 'Suspendisse placerat neque neque. Morbi dictum nulla non sapien rhoncus, et mattis erat commodo. Aliquam vel lacus a justo mollis luctus. Proin vel auctor eros, sed eleifend nunc. Curabitur eget tincidunt risus. Mauris malesuada facilisis magna, vitae volutpat sapien tristique eu. Morbi metus nunc, interdum in erat placerat, aliquam iaculis massa. Duis vulputate varius justo pharetra maximus. In vehicula enim nec nibh porta tincidunt. Vestibulum at ultrices turpis, non dictum metus. Vivamus ligula ex, semper vitae eros ut, euismod convallis augue. Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.',
+        title_2: 'Como tudo começou...',
+        start: 'Suspendisse placerat neque neque. Morbi dictum nulla non sapien rhoncus, et mattis erat commodo. Aliquam vel lacus a justo mollis luctus. Proin vel auctor eros, sed eleifend nunc. Curabitur eget tincidunt risus. Mauris malesuada facilisis magna, vitae volutpat sapien tristique eu. Morbi metus nunc, interdum in erat placerat, aliquam iaculis massa. Duis vulputate varius justo pharetra maximus. In vehicula enim nec nibh porta tincidunt. Vestibulum at ultrices turpis, non dictum metus. Vivamus ligula ex, semper vitae eros ut, euismod convallis augue. Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.',
+        title_3: 'Nossas receitas',
+        recepis: 'Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.'
+    }
+
+    return res.render('about', { data })
 })
 
 app.get('/recepis', function(req, res) {
     return res.render('recepis', { data })
 })
 
-app.get('/recepi:id', function(req, res) {
-    return res.send('recepi')
+app.get('/recepi', function(req, res) {
+    const id = req.query.id;
+
+    const recepi = data.find(function(recepi) {
+        return recepi.id == id
+    })
+
+    if (!recepi) {
+        return res.send('Recepi not found! :(')
+    }
+
+    return res.render('recepi', { item: data })
 })
+
+app.get("/recipes/:index", function (req, res) {
+    const recipes = data; // Array de receitas carregadas do data.js
+    const recipeIndex = req.params.index;
+  
+    console.log(recipes[recipeIndex])
+    return res.render('recepis', { data })
+  })
 
 app.listen(5000, function() {
     console.log('server is running!')
