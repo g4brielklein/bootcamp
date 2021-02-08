527574938ed7cbcbedc0bca270cb2dd5cfe6388c
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-02-08 19:33:01 -0300

    Desafio 3: Refatorando Foodfy

--- foodfy/public/script.js
+++ foodfy/public/script.js
@@ -16,6 +16,7 @@ for (let button of buttons) {
         if (button.classList.contains('hide')) {
             button.innerHTML = "ESCONDER"
             button.classList.remove('hide')
+            content[0]
         } else {
             button.classList.add('hide')
             button.innerHTML = "MOSTRAR"
@@ -25,6 +26,15 @@ for (let button of buttons) {
 }
 for (let content of contents) {
     content.addEventListener('click', function() {
-        alert('clicks')
+        alert('clicou na content')
     })
 }
+
+function acao(){
+    var submenu = document.getElementById('submenu');
+    if(submenu.style.display == 'block'){
+      submenu.style.display = 'none';
+    }else{
+      submenu.style.display = 'block';
+    }
+    }
