4777910b4e9f3446b3937aee07427b66846821bc
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-02-09 20:31:31 -0300

    Desafio 3: Refatorando Foodfy

--- foodfy/public/script.js
+++ foodfy/public/script.js
@@ -1,6 +1,8 @@
+
 const recipes = document.querySelectorAll('.recipe')
 const details = document.querySelector('.recipe-details')
 const contents = document.querySelectorAll('.button-content')
+// var content = document.getElementById('content')
 
 for (let i = 0; i < recipes.length; i++) {
     recipes[i].addEventListener('click', function() {
@@ -16,14 +18,17 @@ for (let button of buttons) {
         if (button.classList.contains('hide')) {
             button.innerHTML = "ESCONDER"
             button.classList.remove('hide')
+            // content.style.display = 'block'
             content[0]
         } else {
             button.classList.add('hide')
             button.innerHTML = "MOSTRAR"
+            // content.style.display = 'none'
         }
 
     })
 }
+
 for (let content of contents) {
     content.addEventListener('click', function() {
         alert('clicou na content')
@@ -32,9 +37,20 @@ for (let content of contents) {
 
 function acao(){
     var submenu = document.getElementById('submenu');
+
     if(submenu.style.display == 'block'){
-      submenu.style.display = 'none';
-    }else{
-      submenu.style.display = 'block';
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
     }
+}
+
+function action() {
+    var content = document.getElementById('content')
+
+    if (content.style.display == 'block') {
+        content.style.display = 'none'
+    } else {
+        content.style.display = 'block'
     }
+}
