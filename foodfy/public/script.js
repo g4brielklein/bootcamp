df7f703d9f77a4232d56f8f5614dab7d6f8bf575
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-02-10 09:02:53 -0300

    Desafio 3: Refatorando Foodfy

--- foodfy/public/script.js
+++ foodfy/public/script.js
@@ -1,8 +1,6 @@
-
 const recipes = document.querySelectorAll('.recipe')
-const details = document.querySelector('.recipe-details')
 const contents = document.querySelectorAll('.button-content')
-// var content = document.getElementById('content')
+const buttons = document.querySelectorAll('.button')
 
 for (let i = 0; i < recipes.length; i++) {
     recipes[i].addEventListener('click', function() {
@@ -10,47 +8,16 @@ for (let i = 0; i < recipes.length; i++) {
     })
 }
 
-const buttons = document.querySelectorAll('.button')
-
-for (let button of buttons) {
-    button.addEventListener('click', function() {
-
-        if (button.classList.contains('hide')) {
-            button.innerHTML = "ESCONDER"
-            button.classList.remove('hide')
-            // content.style.display = 'block'
-            content[0]
+for (let i = 0; i < 3; i++) {
+    buttons[i].addEventListener('click', function() {
+        if (buttons[i].classList.contains('hide')) {
+            buttons[i].innerHTML = "ESCONDER"
+            buttons[i].classList.remove('hide')
+            contents[i].style.display = 'block'
         } else {
-            button.classList.add('hide')
-            button.innerHTML = "MOSTRAR"
-            // content.style.display = 'none'
+            buttons[i].classList.add('hide')
+            buttons[i].innerHTML = "MOSTRAR"
+            contents[i].style.display = 'none'
         }
-
     })
-}
-
-for (let content of contents) {
-    content.addEventListener('click', function() {
-        alert('clicou na content')
-    })
-}
-
-function acao(){
-    var submenu = document.getElementById('submenu');
-
-    if(submenu.style.display == 'block'){
-        submenu.style.display = 'none';
-    } else {
-        submenu.style.display = 'block';
-    }
-}
-
-function action() {
-    var content = document.getElementById('content')
-
-    if (content.style.display == 'block') {
-        content.style.display = 'none'
-    } else {
-        content.style.display = 'block'
-    }
 }
