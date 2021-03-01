f9cb7f5fbbbcb1e823a02ac1efd7d42e8228bd08
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-03-01 20:36:39 -0300

    Desafio 4-3: Formulário e rota de cadastro do professor

--- modulo04/desafios/desafio-4-3/routes.js
+++ modulo04/desafios/desafio-4-3/routes.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const routes = express.Router()
 const teachers = require('./teachers')
+const data = require('./data.json')
 
 routes.get('/', function(req, res) {
     return res.redirect('teachers')
 })
 
 routes.get('/teachers', function(req, res) {
-    return res.render('teachers/index') 
+    return res.render('teachers/index', { data }) 
 })
 
 routes.get('/students', function(req, res) {
