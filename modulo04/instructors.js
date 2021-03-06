bac609028eac90ae4381fff4b18cc43ac772b65d
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-03-06 12:10:32 -0300

    Formatando data com Intl do Javascript

--- modulo04/instructors.js
+++ modulo04/instructors.js
@@ -15,17 +15,14 @@ exports.show = function(req, res) {
     }
 
     function created_at(timestamp) {
-        const creation_date = new Date(timestamp).toLocaleDateString('pt-BR')
-
-        return creation_date
+        return creation_date = new Date(timestamp).toLocaleDateString('pt-BR')
     }
     
     const instructor = {
-        // Spread Operator
-        ...foundInstructor,
+        ...foundInstructor, // Spread Operator
         age: age(foundInstructor.birth),
         services: foundInstructor.services.split(','),
-        created_at: created_at(foundInstructor.created_at)
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at)
     }
 
     return res.render('instructors/show', { instructor })
