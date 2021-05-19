943d771b0036b18c4159f8caefebd43b36652c68
g4brielklein <gabrielkleinribas@gmail.com>
2021-05-18 22:50:07 -0300

    Desafio 4-3

--- modulo04/desafios/desafio-4-3/server.js
+++ modulo04/desafios/desafio-4-3/server.js
@@ -14,6 +14,24 @@ nunjucks.configure('views', {
     noCache: true
 })
 
+server.get('/', function(req, res) {
+    const data = {
+        avatar_url: 'https://avatars0.githubusercontent.com/u/22963494?s=460&u=3f020d4a9f9041ada4585a645cd8721b99a9fcc0&v=4',
+        name: 'Gabriel Klein',
+        role: 'Data Engineer at <a href="http://cadastra.com" target="_blank">Cadastra</a>',
+        description: 'Student at Rocketseat and Information Tecnology Student at Ulbra',
+        links: [
+            {
+                url: 'https://github.com/g4brielklein',
+                name: 'GitHub'
+            },
+            {
+                url: 'https://linkedin.com/in/g4brielklein',
+                name: 'LinkedIn'
+            }
+        ]
+    }
+
 server.listen(5000, function() {
     console.log('Server is running!')
 })
