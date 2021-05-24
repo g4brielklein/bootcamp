10c0832b547f392c18ce31a48cb9e374fed9d376
g4brielklein <gabrielkleinribas@gmail.com>
2021-05-24 00:08:25 -0300

    Testing

--- modulo04/test.js
+++ modulo04/test.js
@@ -4,7 +4,9 @@ const notebook = {
     year: 2017,
     price: 8.499,
     os: "MacOS Big Sur",
-    screen_size: "13.3 inches"
+    screen_size: "13.3 inches",
+    ram: "8gb",
+    processor: "Apple M1"
 }
 
 console.log(notebook)
