b5194dcae4ac3b4f3afb519d2fbcc11155ebc98c
g4brielklein <gabrielkleinribas@gmail.com>
2021-07-08 00:07:24 -0300

    Criando datas no JavaScript

--- modulo04/instructors.js
+++ modulo04/instructors.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const data = require('./data.json')
+
 // create
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
@@ -8,5 +11,17 @@ exports.post = function(req, res) {
         }
     }
 
-    return res.send(req.body)
+    req.body.birth = Date.parse(req.body.birth)
+    req.body.created_at = Date.now()
+    data.instructors.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) {
+            return res.send("Write file error")
+        }
+
+        return res.redirect("/instructors")
+    })
+
+    // return res.send(req.body)
 }
