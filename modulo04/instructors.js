7e8ee5e9d5c4d469519218f96d574f18de66e8cf
g4brielklein <gabrielkleinribas@gmail.com>
2021-07-09 20:25:15 -0300

    Estruturando dados de instrutores

--- modulo04/instructors.js
+++ modulo04/instructors.js
@@ -11,9 +11,21 @@ exports.post = function(req, res) {
         }
     }
 
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
-    data.instructors.push(req.body)
+    let {avatar_url, name, birth, gender, services} = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id 
+        , avatar_url
+        , name
+        , birth
+        , gender
+        , services
+        , created_at
+    }) 
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) {
