42bd880ac214e469127a04b2ed3eaa2960055bf7
g4brielklein <gabrielkleinribas@gmail.com>
2021-07-15 22:16:38 -0300

    Formatando data com intl do JavaScript

--- modulo04/instructors.js
+++ modulo04/instructors.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
+const { age } = require('./utils')
 
 // create
 exports.post = function(req, res) {
@@ -52,9 +53,9 @@ exports.show = function(req, res) {
 
     const instructor = {
         ...foundInstructor
-        , age: ""
+        , age: age(foundInstructor.birth)
         , services: foundInstructor.services.split(", ")
-        , created_at: ""
+        , created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
     }
 
     return res.render('instructors/show', { instructor })
