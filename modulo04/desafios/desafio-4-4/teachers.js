b6daf53dc3c24a90d4fec60e657fd3e7b8590647
g4brielklein <gabrielkleinribas@gmail.com>
2021-07-22 22:33:34 -0300

    Desafio 4-4: Apresentação, edição e formatação dos dados de um professor

---
+++ modulo04/desafios/desafio-4-4/teachers.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const { url } = require('inspector')
+const data = require('./data.json')
+
+// create
+exports.post = function(req, res) {
+
+    const keys = Object.keys(req.body)
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send("Fill all fields")
+        }
+    }
+
+    let { avatar_url, name, birth, education, modality, classes } = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.teachers.length + 1)
+
+    data.teachers.push({
+        id
+        , avatar_url
+        , name
+        , birth
+        , education
+        , modality
+        , classes
+        , created_at
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) {
+            return res.send('Error writing file')
+        }
+
+        return res.redirect("/teachers")
+    })
+}
+
+// show
+exports.show = function(req, res) {
+    const id = req.params.id;
+
+    let foundTeacher = 0;
+
+        for (teacher of data.teachers) {
+            if (teacher.id == id) {
+                foundTeacher = teacher
+                return res.render('teachers/show', {teacher: foundTeacher})
+            }
+        }
+
+    return res.send('Teacher not found.')
+}
