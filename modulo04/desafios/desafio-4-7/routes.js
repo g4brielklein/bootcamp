ab9083b895129a33d6d80c650ed780aac789cc26
g4brielklein <gabrielkleinribas@gmail.com>
2021-09-07 23:16:58 -0300

    Desafio 4-7: Estruturando estudantes

---
+++ modulo04/desafios/desafio-4-7/routes.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const routes = express.Router()
+const teachers = require('./teachers')
+
+routes.get('/', function(req, res) {
+    return res.redirect("/teachers")
+})
+
+routes.get('/teachers', teachers.index)
+
+routes.get('/students', function(req, res) {
+    return res.send("students")
+})
+
+routes.get('/teachers/create', function(req, res) {
+    return res.render('teachers/create')
+})
+
+routes.get('/teachers/:id', teachers.show)
+
+routes.get('/teachers/:id/edit', teachers.edit)
+
+routes.put('/teachers', teachers.update)
+
+routes.delete('/teachers', teachers.delete)
+
+routes.post('/teachers', teachers.post)
+
+module.exports = routes
