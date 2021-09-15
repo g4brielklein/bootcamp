f4cc03beb3db8e96f2c538a9c8d71f6c1a2c98a4
g4brielklein <gabrielkleinribas@gmail.com>
2021-09-14 23:18:49 -0300

    Desafio 4-7: Estruturando estudantes

--- modulo04/desafios/desafio-4-7/public/script.js
+++ modulo04/desafios/desafio-4-7/public/script.js
@@ -6,3 +6,12 @@ for (item of menuItems) {
         item.classList.add("active")
     }
 }
+
+// Delete confirmation message
+const formDelete = document.querySelector("#form-delete")
+formDelete.addEventListener("submit", function(event) {
+    const confirmation = confirm("Do you really want to delete this user?")
+    if (!confirmation) {
+        event.preventDefault()
+    }
+})
