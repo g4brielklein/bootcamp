39ca17572191f473c1328c4a87be5d729ac41aad
g4brielklein <gabrielkleinribas@gmail.com>
2021-09-27 20:44:48 -0300

    Desafio: Administração do Foodfy

--- foodfy/server.js
+++ foodfy/server.js
@@ -1,45 +1,16 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const data = require('./data')
+const user = require('./controllers/user')
+const admin = require('./controllers/admin')
 
-const app = express()
-
+const routes = express()
 app.use(express.static('public'))
-
 app.set('view engine', 'njk')
-
 nunjucks.configure('views', {
     express:app,
     autoescape: false,
     noCache: true
 })
-
-app.get('/', function(req, res)  {
-    return res.render('index', { data })
-})
-
-app.get('/about', function(req, res) {
-    const data = {
-        contents: [
-            { title: 'Sobre o Foodfy', text: 'Suspendisse placerat neque neque. Morbi dictum nulla non sapien rhoncus, et mattis erat commodo. Aliquam vel lacus a justo mollis luctus. Proin vel auctor eros, sed eleifend nunc. Curabitur eget tincidunt risus. Mauris malesuada facilisis magna, vitae volutpat sapien tristique eu. Morbi metus nunc, interdum in erat placerat, aliquam iaculis massa. Duis vulputate varius justo pharetra maximus. In vehicula enim nec nibh porta tincidunt. Vestibulum at ultrices turpis, non dictum metus. Vivamus ligula ex, semper vitae eros ut, euismod convallis augue. Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.' },
-            { title: 'Como tudo começou...', text: 'Suspendisse placerat neque neque. Morbi dictum nulla non sapien rhoncus, et mattis erat commodo. Aliquam vel lacus a justo mollis luctus. Proin vel auctor eros, sed eleifend nunc. Curabitur eget tincidunt risus. Mauris malesuada facilisis magna, vitae volutpat sapien tristique eu. Morbi metus nunc, interdum in erat placerat, aliquam iaculis massa. Duis vulputate varius justo pharetra maximus. In vehicula enim nec nibh porta tincidunt. Vestibulum at ultrices turpis, non dictum metus. Vivamus ligula ex, semper vitae eros ut, euismod convallis augue. Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.' },
-            { title: 'Nossas receitas', text: 'Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.' }
-        ]
-    }
-
-    return res.render('about', { data })
-})
-
-app.get('/recipes', function(req, res) {
-    return res.render('recipes', { data })
-})
-
-app.get("/recipe/:index", function (req, res) {
-    const recipes = data;
-    const recipeIndex = req.params.index;
-  
-    return res.render('recipe', { item: recipes[recipeIndex] })
-  })
 
 app.listen(5000, function() {
     console.log('Server is running!')
