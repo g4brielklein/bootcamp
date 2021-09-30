52760aa4677a962e135efbf6518169dbe1296e40
g4brielklein <gabrielkleinribas@gmail.com>
2021-09-30 18:11:04 -0300

    Desafio: Administração do Foodfy

--- foodfy/controllers/recipes.js
+++ foodfy/controllers/recipes.js
@@ -13,3 +13,15 @@ exports.show = function (res, res) {
 exports.edit = function(req, res) {
     return res.send('// Mostrar formulário de edição de receita')
 }
+
+exports.post = function (req, res) {
+    return res.send('// Cadastrar nova receita')
+}
+
+exports.put = function (req, res) {
+    return res.send('// Editar uma receita')
+}
+
+exports.delete = function (req, res) {
+    return res.send('// Deletar uma receita')
+}
